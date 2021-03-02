8a7983412a24ba7265db67b93e8ce2ffe3f948bd
ruhul rabby <ruhulalam1994@gmail.com>
2021-03-02 17:29:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Country from './Components/Country/Country';
+ 
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(res => res.json())
+    .then(data => {
+      setCountries(data);
+      data.map(country=> country.name)
+    })
+  }, [])
+
+  const handleAddCountry = (country) => {
+    console.log('country added',country)};
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>country loaded : {countries.length}</h1>
+      <h4>Country added: </h4>
+      <ul>
+      {
+        countries.map(country => <Country country={country} handleAddCountry= {handleAddCountry} key= {country.alpha3Code} >
+        </Country> )
+      }
+      </ul>
     </div>
   );
 }
