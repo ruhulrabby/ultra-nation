b798a3939caf7dc45c5969bf78179a1b827a8857
ruhul rabby <ruhulalam1994@gmail.com>
2021-03-02 17:59:30 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Cart from './Components/Cart/Cart';
 import Country from './Components/Country/Country';
  
 
 function App() {
 
   const [countries, setCountries] = useState([]);
-
+  const [cart, setCart] = useState([]); 
+  
   useEffect(()=>{
     fetch("https://restcountries.eu/rest/v2/all")
     .then(res => res.json())
@@ -17,12 +19,16 @@ function App() {
   }, [])
 
   const handleAddCountry = (country) => {
-    console.log('country added',country)};
+    const newCart = [...cart, country];
+    setCart(newCart);
+  };
+ 
   
   return (
     <div className="App">
       <h1>country loaded : {countries.length}</h1>
-      <h4>Country added: </h4>
+      <h4>Country added: {cart.length}</h4>
+      <Cart cart= {cart} ></Cart>
       <ul>
       {
         countries.map(country => <Country country={country} handleAddCountry= {handleAddCountry} key= {country.alpha3Code} >
